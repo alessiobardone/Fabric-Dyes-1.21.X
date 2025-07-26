8fff5b6851a8883ba73865c7bac671c83c9a5863
alessiobardone <alessiobardone03@gmail.com>
2025-07-26 15:53:03 +0200

    Test

--- src/main/java/net/brdviii/dyes/Dyes.java
+++ src/main/java/net/brdviii/dyes/Dyes.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//ciao
 public class Dyes implements ModInitializer {
 	public static final String MOD_ID = "dyes";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
